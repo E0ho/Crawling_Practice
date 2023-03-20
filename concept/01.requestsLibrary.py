8158233571bc61d61adb417f2fa13206dd4b1189
Youngho Lee <bbchoi6@hufs.ac.kr>
2023-03-20 16:36:21 +0900

    간단한 개념 활용 실습

--- concept/01.requestsLibrary.py
+++
@@ -1,9 +0,0 @@
-# requests(http 통신을 위한 파이썬 라이브러리) 라이브러리 파일로 불러오기
-import requests
-
-# 특정 url 받아오기
-response = requests.get("https://www.naver.com")
-
-# 응답중 html 코드 가져오려면 .text 사용
-html = response.text
-print(html)
